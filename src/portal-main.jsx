04b2e91eaa9b6cb4c80ad0b8877798fc75827415
RemolquesNorte <logistica@trailer.com.mx>
2026-07-15 14:33:16 -0600

    Delete src/portal-main.jsx

--- src/portal-main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Portal from './Portal.jsx';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Portal />
-  </React.StrictMode>
-);
